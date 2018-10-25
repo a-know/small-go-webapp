c89a34a43ead90b7ec2a59d459adb7703dae13c0
a-know <a-know@hatena.ne.jp>
2018-10-25 14:32:15 +0900

    small webapp

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Get("/heartbeat", HandleHeartbeat)
+
+	log.Printf("small-app started.")
+	http.ListenAndServe(":8080", r)
+}
